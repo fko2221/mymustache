f6d71f78c4f1a4d905a1749557a0bab1ac8b6279
Kent Oyer <kent.oyer@gmail.com>
2014-08-29 23:17:51 -0400

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ mymustache
 ==========
 
 An enhancement to the PHP Mustache class
+
+Features
+--------
+
+- FILTERS Pragma is enabled by default
+- Default template file name extension is .htm
--- mymustache.php
+++ mymustache.php
@@ -14,10 +14,13 @@ class MyMustache extends Mustache_Engine
 		$options =  array('extension' => '.htm');
 		parent::__construct(array(
 			'loader' => new Mustache_Loader_FilesystemLoader($path, $options),
+			'pragmas' => array(Mustache_Engine::PRAGMA_FILTERS)
 		));
 		
 		parent::addHelper('$',function($value) { return '$'.@number_format($value,2); });
 		parent::addHelper(',',function($value) { return @number_format($value,2); });
+		parent::addHelper('UC',function($value) { return @strtoupper((string)$value); });
+		parent::addHelper('LC',function($value) { return @strtolower((string)$value); });
 		
 	}
 
