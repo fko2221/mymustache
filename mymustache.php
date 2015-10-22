becb9b7e80df61b72046a0f7913d3e96886c2720
Kent Oyer <kent.oyer@gmail.com>
2015-10-22 02:30:18 +0000

    Added more helpers

--- mymustache.php
+++ mymustache.php
@@ -19,8 +19,12 @@ class MyMustache extends Mustache_Engine
 		
 		parent::addHelper('$',function($value) { return '$'.@number_format($value,2); });
 		parent::addHelper(',',function($value) { return @number_format($value,2); });
+		parent::addHelper('%',function($value) { return ($value == 0 ? '' : ($value*100).'%' ); });
 		parent::addHelper('UC',function($value) { return @strtoupper((string)$value); });
 		parent::addHelper('LC',function($value) { return @strtolower((string)$value); });
+		parent::addHelper('MDY',function($value) { return @date('m/d/Y',strtotime((string)$value)); });
+		parent::addHelper('NL2BR',function($value) { return @nl2br((string)$value); });
+		parent::addHelper('JSON',function($value) { return @json_encode($value); });
 		
 	}
 
